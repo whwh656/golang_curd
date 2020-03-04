f20aff5e254dc1c401ca282d514aa9ae07eb9d9a
王洪02 <wanghong02@qutoutiao.net>
2020-03-04 21:21:40 +0800

    add tad to no show some fileds in struct to response

--- model/user.go
+++ model/user.go
@@ -4,11 +4,11 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// User user对象 如何迁移结构体到表中 属性首字母大写不然外不访问不到
+// User user对象 如何迁移结构体到表中 属性首字母大写不然外不访问不到`json:",omitempty"`
 type User struct {
-	gorm.Model
-	Name string
-	Age  int
+	gorm.Model  
+	Name string `json:"-"`
+	Age  int    
 }
 
 var user User
